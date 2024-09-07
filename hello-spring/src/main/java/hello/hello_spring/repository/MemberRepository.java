5fe9b82114c27e9f671025f691b5dc446ddb9d55
KangMinChang <minchanggang841@gmail.com>
2024-09-07 12:45:14 +0900

    회원 레파지토리 생성

---
+++ hello-spring/src/main/java/hello/hello_spring/repository/MemberRepository.java
@@ -0,0 +1,14 @@
+package hello.hello_spring.repository;
+
+import hello.hello_spring.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+    //null을 그대로 처리하는 대신 Optional로 감싸서 처리
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+}

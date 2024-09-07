13d06c2ab7b928331ed322c490e84f4d23a593e8
KangMinChang <minchanggang841@gmail.com>
2024-09-07 12:21:31 +0900

    API / String , JSON

--- hello-spring/src/main/java/hello/hello_spring/controller/HelloController.java
+++ hello-spring/src/main/java/hello/hello_spring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -13,6 +14,7 @@ public class HelloController {
         model.addAttribute("data", "hello!!");
         return "hello";
     }
+    // Spring MVC
     @GetMapping("hello-mvc")
     public String helloMvc(@RequestParam(name="name",required = false) String name, Model model){
         //required = false 일 경우 name 값이 없어도 오류가 나지 않음
@@ -20,4 +22,32 @@ public class HelloController {
         return "hello-template";
     }
 
+    //API
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam(name="name",required = false) String name){
+        //ResponseBody 사용시 뷰리졸버를 사용하지 않음
+        return " hello " + name;
+    }
+    //문자가 아니고 객체일 경우 JSON 방식으로 나온다.
+    //객체일 경우 jsonconverter 동작 string일경우 stringconverter 동작
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam(name="name",required = false) String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    public static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
 }
